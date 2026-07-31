ac5a9597ee61877a24f131b1e44d3f385a4a3010
Tim <28957605+turulix@users.noreply.github.com>
2026-07-31 18:31:16 +0200

    more improvements

--- home/programs/bitwarden-desktop/default.nix
+++ home/programs/bitwarden-desktop/default.nix
@@ -1,5 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs; [
     bitwarden-desktop
   ];
+
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock";
+  };
 }
