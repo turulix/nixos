59675606a97c5d4353f39e8207fb95f7f7529cd1
Tim <28957605+turulix@users.noreply.github.com>
2026-07-31 22:22:52 +0200

    REEEEEEEEEEE

--- home/programs/helix/default.nix
+++ home/programs/helix/default.nix
@@ -1,46 +1,53 @@
 {
-  config,
   pkgs,
-  lib,
   osConfig,
   ...
 }:
-let
-in
 {
   programs.helix = {
     enable = true;
     defaultEditor = true;
 
     extraPackages = with pkgs; [
+      nil
       nixd
+      nixfmt
     ];
 
     languages = {
       language-server.nixd = {
-        command = "nixd";
-        args = [ "--semantic-tokens=true" ];
-
-        config.nixd =
+        command = "${pkgs.nixd}/bin/nixd";
+        config =
           let
             myFlake = ''(builtins.getFlake "/etc/nixos")'';
             nixosOpts = "${myFlake}.nixosConfigurations.${osConfig.networking.hostName}.options";
           in
           {
             nixpkgs.expr = "import ${myFlake}.inputs.nixpkgs { }";
-            formatting.command = [ "${lib.getExe pkgs.nixfmt}" ];
             options = {
               nixos.expr = nixosOpts;
               home-manager.expr = "${nixosOpts}.home-manager.users.type.getSubOptions []";
             };
           };
       };
+
       language = [
         {
           name = "nix";
+          formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
           auto-format = true;
+          language-servers = [
+            "nil"
+            "nixd"
+          ];
         }
       ];
+    };
+
+    settings = {
+      editor = {
+        auto-format = true;
+      };
     };
   };
 }
